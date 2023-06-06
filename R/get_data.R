7c3e3fd94512c59ddb479135cd6a2f5061e2ad86
Christopher Belanger <christopher@belangeranalytics.com>
2023-06-06 09:18:25 -0400

    update buffer and bbox, add more recent data

--- R/get_data.R
+++ R/get_data.R
@@ -6,8 +6,13 @@ library(neighbourhoodstudy)
 library(sf)
 
 ons_gen3_shp <- dplyr::filter(neighbourhoodstudy::ons_gen3_shp, ONS_Region == "OTTAWA")
+ons_gen3_buffer <- ons_gen3_shp |>
+  sf::st_transform(ons_gen3_shp, crs=32189) |>
+  sf::st_union() |>
+  sf::st_buffer(10000) |>
+  sf::st_transform(crs="WGS84")
 
-osm_query <- osmdata::opq(bbox = sf::st_bbox(ons_gen3_shp), timeout = 10000)
+osm_query <- osmdata::opq(bbox = sf::st_bbox(ons_gen3_buffer), timeout = 10000)
 
 amenities <- c("restaurant",
                "pub",
@@ -20,7 +25,6 @@ shops <- c("grocery",
            "convenience",
            "deli",
            "greengrocer",
-           "deli",
            "food",
            "general",
            "supermarket")
@@ -29,6 +33,10 @@ shops_query <- paste0("\"shop\"=\"", shops, "\"" )
 
 amenities_query <- paste0("\"amenity\"=\"", amenities, "\"" )
 
+
+#bboxes <- make_buffer_grid(ons_gen3_buffer, side_length=2)
+
+#osm_query <- osmdata::opq(bbox = bboxes[[1]], timeout = 10000)
 osm_query <- osmdata::add_osm_features(osm_query, features = c(shops_query, amenities_query))
 
 #osm_query <- osmdata::add_osm_features(osm_query, features = amenities_query)
@@ -50,7 +58,8 @@ osm_pts <- osm_pts |>
   sf::st_as_sf() |>
   dplyr::mutate(type = dplyr::if_else(is.na(amenity), shop, amenity)) |>
   dplyr::select(osm_id, type, name, shop, amenity, brand, addr.street, addr.housenumber, addr.city, addr.postcode) |>
-  dplyr::filter(!is.na(type))
+  dplyr::filter(!is.na(type))#|>
+  #sf::st_transform(crs = "WGS84")
 
 osm_poly_centroids <- osm_polys |>
   dplyr::as_tibble() |>
@@ -58,14 +67,17 @@ osm_poly_centroids <- osm_polys |>
   dplyr::mutate(type = dplyr::if_else(is.na(amenity), shop, amenity)) |>
   dplyr::select(osm_id, type, name, shop, amenity, brand, addr.street, addr.housenumber, addr.city, addr.postcode) |>
   dplyr::filter(!is.na(type)) |>
-  sf::st_centroid()
+  sf::st_centroid()#|>
+  #sf::st_transform(crs = "WGS84")
 
 
 ottawa_food <- dplyr::bind_rows(osm_pts, osm_poly_centroids) |>
-  sf::st_transform(crs = "WGS84") |>
-  sf::st_filter(ons_gen3_shp)
+  sf::st_transform(crs = "WGS84")
 
 sf::write_sf(ottawa_food, paste0("data/ottawa-osm-food-", Sys.Date(), ".shp"))
+
+
+ottawa_food <- sf::read_sf("data/ottawa-osm-food-2023-05-20.shp")
 
 ottawa_food %>%
   dplyr::bind_cols(., sf::st_coordinates(.)) |>
@@ -108,3 +120,40 @@ ottawa_food |>
 sf::st_crs(ottawa_food)
 
 sf::st_crs(ons_gen3_shp)
+
+
+
+
+bounding_box <- sf::st_bbox(ons_gen3_buffer)
+
+# if our bounding box is too big, split it into smaller ones
+make_buffer_grid <- function(bounding_box, side_length=2) {
+
+  bboxes <- list()
+  xmin_orig <- bounding_box$xmin
+  ymin_orig <- bounding_box$ymin
+  xmax_orig <- bounding_box$xmax
+  ymax_orig <- bounding_box$ymax
+
+  xlength <- xmax_orig - xmin_orig
+  ylength <- ymax_orig - ymin_orig
+
+  for (i in 1:side_length){
+
+    for (j in 1:side_length){
+
+      xmin <- xmin_orig + (i-1) * xlength
+      xmax <- xmin_orig + i * xlength
+      ymin <- ymin_orig + (j-1) * ylength
+      ymax <- ymin_orig + j * ylength
+
+      bbox <- c(xmin, ymin, xmax, ymax)
+      names(bbox) <- c("xmin", "ymin", "xmax", "ymax")
+
+    bboxes[[(i-1) * side_length + j]] <- bbox
+
+    }
+  }
+
+bboxes
+}
